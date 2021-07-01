78958e63d7b441ed2588de379adfce003bee4911
HJTN <zintn2012@gmail.com>
2021-07-01 12:29:45 +0900

    Login information transform system

--- app/src/main/java/com/example/example1/MainActivity.java
+++ app/src/main/java/com/example/example1/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.example1;
 
+import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextInputEditText TextInputEditText_email, TextInputEditText_password;
+    RelativeLayout RelativeLayout_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextInputEditText_email     = findViewById(R.id.TextInputEditText_email);
+        TextInputEditText_password  = findViewById(R.id.TextInputEditText_password);
+        RelativeLayout_login        = findViewById(R.id.RelativeLayout_login);
+
+        // 1. 값을 가져옴
+        // 2. 클릭을 감지
+        // 3. 1번의 값을 다음 액티비티로 넘김
+
+        RelativeLayout_login.setClickable(true);
+        RelativeLayout_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = TextInputEditText_email.getText().toString();
+                String password = TextInputEditText_password.getText().toString();
+
+                Intent intent = new Intent(MainActivity.this, LoginResultActivity.class);
+                intent.putExtra("email", email);
+                intent.putExtra("password", password);
+                startActivity(intent);
+            }
+        });
     }
 }
