8eb889be3f7f9d4f03e652773b10192c26e477ca
HJTN <zintn2012@gmail.com>
2021-07-04 14:45:43 +0900

    Using Picasso tools and ImageButton(home) created

--- app/src/main/java/com/example/layoutpractice/MainActivity.java
+++ app/src/main/java/com/example/layoutpractice/MainActivity.java
@@ -8,6 +8,9 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.ImageButton;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +19,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Picasso using Practice!
+        ImageButton IMG_B1 = (ImageButton) findViewById(R.id.IMG_B1);
+        ImageButton IMG_B2 = (ImageButton) findViewById(R.id.IMG_B2);
+        ImageButton IMG_B3 = (ImageButton) findViewById(R.id.IMG_B3);
+        ImageButton IMG_B4 = (ImageButton) findViewById(R.id.IMG_B4);
+
+        Picasso.get().load(R.drawable.home).into(IMG_B1);
+        Picasso.get().load(R.drawable.calendar).into(IMG_B2);
+        Picasso.get().load(R.drawable.build).into(IMG_B3);
+        Picasso.get().load(R.drawable.settings).into(IMG_B4);
+
+        IMG_B1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer);
+                if(drawer.isDrawerOpen(Gravity.LEFT)){
+                    drawer.closeDrawer(Gravity.LEFT);
+                }
+            }
+        });
+
+        // DrawerLayout Practice!
         Button bOpen = (Button)findViewById(R.id.open);
         bOpen.setOnClickListener(new View.OnClickListener() {
             @Override
